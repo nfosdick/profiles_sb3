c8bc3147efe097adfb2d4e6ba5a5f97b154a27e5
root <root@puppet.lark.com>
2017-09-19 19:25:34 +0000

    test

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,5 +1,12 @@
 #
 class profiles::mysql {
   #
+
+  $config = lookup('profile::mysql::config', Hash, deep)
+
   include mysql::server
+
+  $config.each |$index, $value| {
+   notify { $value: message => $index }
+  } 
 }
