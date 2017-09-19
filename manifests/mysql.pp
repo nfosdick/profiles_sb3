76db46d5ccfbd8e237da31484ee6a9d2928b2442
root <root@puppet.lark.com>
2017-09-19 20:25:04 +0000

    test

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -2,7 +2,7 @@
 class profiles::mysql {
   #
 
-  $config = lookup('profiles::mysql::config', Hash, 'deep', {})
+  $config = lookup('profiles::mysql::config', Hash[String], 'deep', {})
   notify{"$config":}
   include mysql::server
 
